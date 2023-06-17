75bbfbb996fc1834517c565cd6ffab141889ca8a
LuckyDayX <vladimirmatvieiev@gmail.com>
2023-06-17 15:09:22 +0300

    all files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://luckydayx.github.io/comfy-patio/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Next page!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
